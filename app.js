b0fa7ced4b0d040567f319951c6df559a19a1b4f
quoccuong205 <quoccuong2599@gmail.com>
2019-12-17 19:49:36 +0700

    add checkout

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var expressHbs = require('express-handlebars');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var passport = require('passport');
-var flash = require('connect-flash');
-var validator = require('express-validator');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var expressHbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var session = require("express-session");
+var passport = require("passport");
+var flash = require("connect-flash");
+var validator = require("express-validator");
+var MongoStore = require("connect-mongo")(session);
 
-var indexRouter = require('./routes/index');
-var userRoutes = require('./routes/user');
+var indexRouter = require("./routes/index");
+var userRoutes = require("./routes/user");
 
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/shopping', { useNewUrlParser: true, useUnifiedTopology: true });
-require('./config/passport');
+mongoose.connect("mongodb://localhost:27017/shopping", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+require("./config/passport");
 
 // view engine setup
-app.engine('.hbs',expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -30,19 +34,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(
+  session({
+    secret: "mysupersecret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 } //3 hours
+  })
+);
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function(req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
-app.use('/user', userRoutes);
-app.use('/', indexRouter);
+app.use("/user", userRoutes);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,11 +66,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,53 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var Cart = require("../models/cart");
 
-var Product = require('../models/product');
+var Product = require("../models/product");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Product.find(function(err, docs) { 
+router.get("/", function(req, res, next) {
+  Product.find(function(err, docs) {
     var productChunks = [];
-    var chunkSize = 6 ;
+    var chunkSize = 6;
     for (var i = 0; i < docs.length; i += chunkSize) {
-      productChunks.push(docs.slice(i,i + chunkSize));
+      productChunks.push(docs.slice(i, i + chunkSize));
     }
-    res.render('shop/index', { title: 'SKYPHONE', products: productChunks });
+    res.render("shop/index", { title: "SKYPHONE", products: productChunks });
   });
+});
+
+router.get("/add-to-cart/:id", function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      return res.redirect("/");
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect("/");
+  });
+});
+
+router.get("/shopping-cart", function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render("shop/shopping-cart", { products: null });
+  }
+  var cart = new Cart(req.session.cart);
+  res.render("shop/shopping-cart", {
+    products: cart.generateArray(),
+    totalPrice: cart.totalPrice
+  });
+});
+
+router.get("/checkout", function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect("/shopping-cart");
+  }
+  var cart = new Cart(req.session.cart);
+  res.render("shop/checkout", { total: cart.totalPrice });
 });
 
 module.exports = router;
