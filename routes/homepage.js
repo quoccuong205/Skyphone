ad03ec35e736a3b33d3c19e4c8b23fd50e7f3719
Quockhanh <quockhanhtn23@gmail.com>
2019-12-19 00:27:28 +0700

    them duong dan den cac danh muc

--- routes/homepage.js
+++ routes/homepage.js
@@ -32,7 +32,97 @@ router.get("/collections/iphone-quoc-te", function(req, res, next) {
       
       productChunks.push(docs1.slice(i, i + chunkSize));
     }
-    res.render("homepage/iphonequocte", { products: productChunks });
+    res.render("homepage/category", { products: productChunks });
+  });
+});
+
+router.get("/collections/dien-thoai-android", function(req, res, next) {
+  Product.find(function(err, docs) {
+    var productChunks = [];
+    var chunkSize = 4;
+    var docs1= [];
+    for (var i = 0; i< docs.length; i++){
+      if (docs[i].model == 'android'){
+        docs1.push(docs[i]);
+      }
+    }
+    for (var i = 0; i < docs1.length; i += chunkSize) {
+      
+      productChunks.push(docs1.slice(i, i + chunkSize));
+    }
+    res.render("homepage/category", { products: productChunks });
+  });
+});
+
+router.get("/collections/may-tinh-bang", function(req, res, next) {
+  Product.find(function(err, docs) {
+    var productChunks = [];
+    var chunkSize = 4;
+    var docs1= [];
+    for (var i = 0; i< docs.length; i++){
+      if (docs[i].model == 'table'){
+        docs1.push(docs[i]);
+      }
+    }
+    for (var i = 0; i < docs1.length; i += chunkSize) {
+      
+      productChunks.push(docs1.slice(i, i + chunkSize));
+    }
+    res.render("homepage/category", { products: productChunks });
+  });
+});
+
+router.get("/collections/dong-ho", function(req, res, next) {
+  Product.find(function(err, docs) {
+    var productChunks = [];
+    var chunkSize = 4;
+    var docs1= [];
+    for (var i = 0; i< docs.length; i++){
+      if (docs[i].model == 'watch'){
+        docs1.push(docs[i]);
+      }
+    }
+    for (var i = 0; i < docs1.length; i += chunkSize) {
+      
+      productChunks.push(docs1.slice(i, i + chunkSize));
+    }
+    res.render("homepage/category", { products: productChunks });
+  });
+});
+
+router.get("/collections/phu-kien", function(req, res, next) {
+  Product.find(function(err, docs) {
+    var productChunks = [];
+    var chunkSize = 4;
+    var docs1= [];
+    for (var i = 0; i< docs.length; i++){
+      if (docs[i].model == 'accessories'){
+        docs1.push(docs[i]);
+      }
+    }
+    for (var i = 0; i < docs1.length; i += chunkSize) {
+      
+      productChunks.push(docs1.slice(i, i + chunkSize));
+    }
+    res.render("homepage/category", { products: productChunks });
+  });
+});
+
+router.get("/collections/loa-va-tai-nghe", function(req, res, next) {
+  Product.find(function(err, docs) {
+    var productChunks = [];
+    var chunkSize = 4;
+    var docs1= [];
+    for (var i = 0; i< docs.length; i++){
+      if (docs[i].model == 'speaker'){
+        docs1.push(docs[i]);
+      }
+    }
+    for (var i = 0; i < docs1.length; i += chunkSize) {
+      
+      productChunks.push(docs1.slice(i, i + chunkSize));
+    }
+    res.render("homepage/category", { products: productChunks });
   });
 });
 
